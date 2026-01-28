84412567fa37a67e4324428f9a213a4fd3816ae7
mahallavr <mahallavr2022@gmail.com>
2026-01-28 15:15:10 +0100

    ok!

--- brush.js
+++ brush.js
@@ -3,33 +3,52 @@ class Brush {
     this.radius = radius;
     this.color = [r, g, b];
     this.prevPos = null;
+    this.frictionOpts = frictionOpts;
 
+    let opts = this._frictionForRadius(this.radius);
     let options = {
       restitution: 0.6,
-      friction: frictionOpts.friction ?? 0.3,
-      frictionAir: frictionOpts.frictionAir ?? 0.02,
-      frictionStatic: frictionOpts.frictionStatic ?? 0.5,
+      friction: opts.friction,
+      frictionAir: opts.frictionAir,
+      frictionStatic: opts.frictionStatic,
     };
     this.body = Bodies.circle(x, y, this.radius, options);
+    this.body.brushRef = this;
+    Composite.add(engine.world, this.body);
+  }
+
+  _frictionForRadius(radius) {
+    let f = (this.frictionOpts.friction ?? 0.01) * radius;
+    return { friction: f, frictionAir: f, frictionStatic: f };
+  }
+
+  respawn(x, y) {
+    Matter.Body.setPosition(this.body, { x, y });
+    Matter.Body.setVelocity(this.body, { x: 0, y: 0 });
+    Matter.Body.setAngularVelocity(this.body, 0);
+  }
+
+  setRandomSize(radiusMin, radiusMax) {
+    let pos = this.body.position;
+    let vel = this.body.velocity;
+    Composite.remove(engine.world, this.body);
+    this.radius = radiusMin + random(radiusMax - radiusMin);
+    let opts = this._frictionForRadius(this.radius);
+    let options = {
+      restitution: 0.6,
+      friction: opts.friction,
+      frictionAir: opts.frictionAir,
+      frictionStatic: opts.frictionStatic,
+    };
+    this.body = Bodies.circle(pos.x, pos.y, this.radius, options);
+    Matter.Body.setVelocity(this.body, vel);
+    this.body.brushRef = this;
     Composite.add(engine.world, this.body);
   }
 
   show() {
     let pos = this.body.position;
 
-    // Draw one segment from previous position to current (accumulates on canvas)
-    if (this.prevPos !== null) {
-      push();
-      noFill();
-      stroke(this.color[0], this.color[1], this.color[2]);
-      strokeWeight(this.radius * 1.2);
-      strokeCap(ROUND);
-      line(this.prevPos.x, this.prevPos.y, pos.x, pos.y);
-      pop();
-    }
-    this.prevPos = { x: pos.x, y: pos.y };
-
-    // Draw the circle (brush head)
     push();
     fill(this.color[0], this.color[1], this.color[2]);
     noStroke();
